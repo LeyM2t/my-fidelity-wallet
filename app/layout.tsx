40645b5be1da58d9fe13690f49f7ccb4cd1df6b7
Matthieu Leycure <adressemailpouria@gmail.com>
2026-04-22 06:57:28 +0200

    Fix PWA start URL and add app icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,21 +20,21 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" }
     ],
     apple: [
       {
         url: "/apple-touch-icon.png",
         sizes: "180x180",
-        type: "image/png",
-      },
-    ],
+        type: "image/png"
+      }
+    ]
   },
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
-    title: "My Fidelity Wallet",
-  },
+    title: "My Fidelity Wallet"
+  }
 };
 
 export const viewport: Viewport = {
